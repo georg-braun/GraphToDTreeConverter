2b7942deb4ee1ac786f572201091f953036b601a
Georg Braun <georg.braun@inform-software.com>
2019-09-01 11:57:51 +0200

    Initial commit

---
+++ FamilyMember.js
@@ -0,0 +1,19 @@
+class FamilyMember {
+    constructor(id, name, gender) {
+      this.connection = undefined;
+      this.parentConnection = undefined;
+      this.id = id;
+      this.name = name;
+      this.gender = gender;
+    }
+  
+    setConnection(newConnection) {
+      this.connection = newConnection;
+    }
+  
+    setParentConnection(newParentConnection) {
+      this.parentConnection = newParentConnection;
+    }
+  }
+
+module.exports = FamilyMember;
